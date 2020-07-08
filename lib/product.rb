e8a5620bce4e1565349dcd03b60a7bc8f915c27d
Garret Wolf <garretwolf26@gmail.com>
2020-07-08 11:11:10 -0400

    Switch order of quantity and unit_price in initialize method

--- lib/product.rb
+++ lib/product.rb
@@ -1,7 +1,7 @@
 class Product
   attr_reader :category, :name, :quantity, :unit_price
 
-  def initialize(category, name, quantity, unit_price)
+  def initialize(category, name, unit_price, quantity)
     @category = category
     @name = name
     @quantity = quantity
