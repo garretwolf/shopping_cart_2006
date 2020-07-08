0c801d824786f68e3c4214fa280ce9610bd3e6d9
Garret Wolf <garretwolf26@gmail.com>
2020-07-08 11:08:42 -0400

    Create product class and setup initialize method

---
+++ lib/product.rb
@@ -0,0 +1,10 @@
+class Product
+  attr_reader :category, :name, :quantity, :unit_price
+
+  def initialize(category, name, quantity, unit_price)
+    @category = category
+    @name = name
+    @quantity = quantity
+    @unit_price = unit_price
+  end
+end
