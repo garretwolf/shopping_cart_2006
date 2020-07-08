dc89273b928b66686df243753a40275d4d0e7fb4
Garret Wolf <garretwolf26@gmail.com>
2020-07-08 11:38:12 -0400

    Pass first test and setup initialize method

--- test/shopping_cart_test.rb
+++ test/shopping_cart_test.rb
@@ -4,4 +4,10 @@ require './lib/product'
 require './lib/shopping_cart'
 
 class ShoppingCartTest < Minitest::Test
+
+  def test_it_exists
+    cart = ShoppingCart.new("King Soopers", "30items")
+
+    assert_instance_of ShoppingCart, cart
+  end
 end
